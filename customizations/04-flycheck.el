e8cf0d4b0d0cfdd1647edac0f57037b00b1cc3b3
Cao Jiafeng <funfriendcjf@gmail.com>
2016-01-27 13:55:44 +0800

    flycheck not check when editing idle

--- customizations/04-flycheck.el
+++ customizations/04-flycheck.el
@@ -5,7 +5,8 @@
 (add-hook 'after-init-hook 'global-flycheck-mode)
 
 (custom-set-variables
- '(flycheck-display-error-delay 0.2))
+ '(flycheck-display-error-delay 0.2)
+ '(flycheck-check-syntax-automatically (quote (save new-line mode-enabled))))
 
 (provide '04-flycheck)
 ;;; 04-flycheck ends here
