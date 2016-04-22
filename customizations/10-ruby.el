cdfa30d9b523b92b33037199b2f038ce53d294df
Cao Jiafeng <funfriendcjf@gmail.com>
2016-04-22 08:43:54 +0800

    rm usage of enh-ruby-mode

--- customizations/10-ruby.el
+++ customizations/10-ruby.el
@@ -3,19 +3,18 @@
 ;; ruby mode config
 
 ;;; Code:
-(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+;; (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
 
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+;; (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+;; (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
 
-(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+;; (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
 ;;; enables outlining for ruby
 ;;; You may also want to bind hide-body, hide-subtree, show-substree
 ;;; show-all, show-children, ... to some keys easy folding and unfolding
-(add-hook 'enh-ruby-mode-hook
+(add-hook 'ruby-mode-hook
           '(lambda ()
-             (setq enh-ruby-deep-indent-paren nil)
              (outline-minor-mode)
              (setq outline-regexp
                    " *\\(def \\|class\\|module\\|describe \\|it \\)")
