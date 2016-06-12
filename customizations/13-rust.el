3a10ecd9a972f4ff90b63f0d6c2b2b93f4b39733
Cao Jiafeng <funfriendcjf@gmail.com>
2016-06-12 17:34:27 +0800

    config for rust

--- customizations/13-rust.el
+++ customizations/13-rust.el
@@ -3,15 +3,26 @@
 ;; rust mode config
 
 ;;; Code:
-(add-hook 'rust-mode-hook 'racer-mode)
-(add-hook 'racer-mode-hook (lambda ()
-                             (setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
-                             (setq racer-rust-src-path (concat (getenv "HOME") "/.dev/rust/rust/src"))
-                             ))
 
+(add-hook 'rust-mode-hook 'racer-mode)
 (add-hook 'racer-mode-hook 'eldoc-mode)
+
+(setenv "CARGO_HOME"
+        (expand-file-name "~/.cargo"))
+(setq racer-rust-src-path
+      (expand-file-name "rust/src"
+                        (file-name-directory load-file-name)))
+
 (add-hook 'racer-mode-hook '(lambda ()
                               (company-mode)
-                              (local-set-key (kbd "TAB") 'company-indent-or-complete-common)
-                              ))
-;;; 12-rust.el ends here
+                              (local-set-key (kbd "TAB")
+                                             'company-indent-or-complete-common)
+                              (setq company-tooltip-align-annotations t)))
+
+;; TODO: git clone rust src when not exists
+;; (when (not (file-directory-p (expand-file-name "./rust/src")))
+;;   (shell-command-to-string "git clone https://github.com/rust-lang/rust.git"))
+
+
+(provide '13-rust)
+;;; 13-rust.el ends here
