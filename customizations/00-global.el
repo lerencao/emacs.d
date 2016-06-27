418f84c9e06d699775eeaf306823482d6de17a3a
Cao Jiafeng <funfriendcjf@gmail.com>
2016-06-27 14:32:22 +0800

    y-or-p alias

--- customizations/00-global.el
+++ customizations/00-global.el
@@ -3,60 +3,6 @@
 
 ;;; Code:
 
-;;; Text mode and Auto Fill mode
-;; The next two lines put Emacs into Text mode
-;; and Auto Fill mode, and are for writers who
-;; want to start writing prose rather than code.
-(setq-default major-mode 'text-mode)
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-(add-hook 'text-mode-hook 'turn-on-flyspell)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(icomplete-mode t)
- '(menu-bar-mode nil)
- '(show-paren-mode t)
- '(global-linum-mode t)
- '(indent-tabs-mode nil)
- '(require-final-newline t)
- '(show-trailing-whitespace t)
- '(tab-width 2)
- '(make-backup-files nil))
-
-(global-hl-line-mode 1)
-;; customize the background color
-(set-face-background 'hl-line "white")
-(set-face-foreground 'highlight nil)
-
-(add-hook 'before-save-hook 'whitespace-cleanup)
-
-(windmove-default-keybindings)
-
-;; magit mode global key set
-(global-set-key (kbd "C-x g") 'magit-status)
-
-;; By default, C-x C-b runs the list-buffers command.
-;; This command lists your buffers in another window.
-;; Since I almost always want to do something in that window,
-;; I prefer the buffer-menu command, which not only lists the buffers,
-;; but moves point into that window.
-(global-set-key (kbd "C-x C-b") 'buffer-menu)
-
-
-;; buffer-move key-bindings
-(global-set-key (kbd "C-c <C-up>")     'buf-move-up)
-(global-set-key (kbd "C-c <C-down>")   'buf-move-down)
-(global-set-key (kbd "C-c <C-left>")   'buf-move-left)
-(global-set-key (kbd "C-c <C-right>")  'buf-move-right)
-
-
-
-;; exec-path-from-shell
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
 
 ;; font config
 ;; (set-face-attribute 'default nil :height 150)
@@ -116,8 +62,71 @@
 
 (load-theme 'solarized-light t)
 
+
+
+;;; Text mode and Auto Fill mode
+;; The next two lines put Emacs into Text mode
+;; and Auto Fill mode, and are for writers who
+;; want to start writing prose rather than code.
+(setq-default major-mode 'text-mode)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'text-mode-hook 'turn-on-flyspell)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(icomplete-mode t)
+ '(menu-bar-mode nil)
+ '(show-paren-mode t)
+ '(global-linum-mode t)
+ '(indent-tabs-mode nil)
+ '(require-final-newline t)
+ '(show-trailing-whitespace t)
+ '(tab-width 2)
+ '(make-backup-files nil))
+
+(global-hl-line-mode 1)
+;; customize the background color
+(set-face-background 'hl-line "white")
+(set-face-foreground 'highlight nil)
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+(windmove-default-keybindings)
+
+;; magit mode global key set
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; By default, C-x C-b runs the list-buffers command.
+;; This command lists your buffers in another window.
+;; Since I almost always want to do something in that window,
+;; I prefer the buffer-menu command, which not only lists the buffers,
+;; but moves point into that window.
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
+
+
+;; buffer-move key-bindings
+(global-set-key (kbd "C-c <C-up>")     'buf-move-up)
+(global-set-key (kbd "C-c <C-down>")   'buf-move-down)
+(global-set-key (kbd "C-c <C-left>")   'buf-move-left)
+(global-set-key (kbd "C-c <C-right>")  'buf-move-right)
+
+
+
+;; exec-path-from-shell
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+
 (setq-default
  whitespace-line-column 80
  whitespace-style '(face lines-tail trailing tabs))
 (add-hook 'prog-mode-hook 'whitespace-mode)
+
+
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 ;;; 00-global.el ends here
