05ebc0aa72dfd2f4d50ab5765909e7b174862d5d
caojiafeng <funfriendcjf@gmail.com>
2017-06-02 15:48:03 +0800

    do not ask me whether it follow symbolic link

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(scroll-preserve-screen-position 1))
+ '(scroll-preserve-screen-position 1)
+ '(vc-follow-symlinks nil)
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
