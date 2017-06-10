75bd27e39446ae0f9bff1e26bfd69c89de4fec03
caojiafeng <funfriendcjf@gmail.com>
2017-06-10 14:37:17 +0800

    fix bar-mode when non graphic

--- init.el
+++ init.el
@@ -109,10 +109,12 @@
 (load custom-file)
 
 
+(if (display-graphic-p)
+    (progn
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1)
+      (menu-bar-mode -1)))
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
 (show-paren-mode 1)
 (global-linum-mode)
 (global-hl-line-mode t)
